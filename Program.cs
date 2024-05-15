778c68f085645359772bdd6d8ab2700f4967ff43
sdztrk <msaidozturk1@gmail.com>
2024-05-16 01:15:51 +0200

    arrays and loops

--- Program.cs
+++ Program.cs
@@ -108,10 +108,173 @@ namespace HelloWorld
             // string name = $"My full name is: {firstName} {lastName}";
             // Console.WriteLine(name);
 
-            string txt = "We are the so-called \"Vikings\" from the north.";
-            Console.WriteLine(txt);
+            // string txt = "We are the so-called \"Vikings\" from the north.";
+            // Console.WriteLine(txt);
 
             // *******************************************************************
+
+            // int x = 10;
+            // int y = 9;
+            // Console.WriteLine( $"x is bigger then y : {x > y}");
+            // int x = 10;
+            // Console.WriteLine(x == 10);
+            // Console.WriteLine(x == 15);
+
+
+            // *******************************************************************
+
+            // int myAge = 25;
+            // int votingAge = 18;
+
+            // if (myAge >= votingAge)
+            // {
+            //     Console.WriteLine("Old enough to vote!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Not old enough to vote.");
+            // }
+
+
+            //else if
+
+            // int time = 22;
+            // if (time < 10)
+            // {
+            //     Console.WriteLine("Good morning.");
+            // }
+            // else if (time < 20)
+            // {
+            //     Console.WriteLine("Good day.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Good evening.");
+            // }
+
+            // *******************************************************************
+
+            // int day = 10;
+            // switch (day)
+            // {
+            //     case 1:
+            //         Console.WriteLine("Monday");
+            //         break;
+            //     case 2:
+            //         Console.WriteLine("Tuesday");
+            //         break;
+            //     case 3:
+            //         Console.WriteLine("Wednesday");
+            //         break;
+            //     case 4:
+            //         Console.WriteLine("Thursday");
+            //         break;
+            //     case 5:
+            //         Console.WriteLine("Friday");
+            //         break;
+            //     case 6:
+            //         Console.WriteLine("Saturday");
+            //         break;
+            //     case 7:
+            //         Console.WriteLine("Sunday");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Looking forward to the Weekend.");
+            //         break;
+            // }
+
+            // *******************************************************************
+
+
+            // int i = 0;
+            // while (i < 5)
+            // {
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+
+
+            // for (int j = 0; j < 5; j++)
+            // {
+
+            //     Console.WriteLine(j);
+
+            // }
+
+            // // Outer loop
+            // for (int i = 1; i <= 3; ++i)
+            // {
+            //     Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+            //     // Inner loop
+            //     for (int j = 1; j <= 3; j++)
+            //     {
+            //         Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
+            //     }
+            // }
+
+            // string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+            // foreach (string i in cars)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // *******************************************************************
+
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     if (i == 4)
+            //     {
+            //         continue; //if i is equel to 4 it skips the value
+            //      //   break;
+            //     }
+            //     Console.WriteLine(i);
+            // }
+
+            // int i = 0;
+            // while (i < 10)
+            // {
+            //     if (i == 4)
+            //     {
+            //         i++;
+            //         continue;
+            //     }
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+
+            //***********************************************************
+
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            int[] numbers = { 12, 15, 17, 20 };
+
+
+            // Console.WriteLine(cars[0]);
+            // cars[0] = "Opel";
+            // Console.WriteLine(cars[0]);
+
+
+            // for (int i = 0; i< cars.Length; i++){
+            //     Console.WriteLine(cars[i]);
+            // }
+
+            // for (int i = numbers.Length-1 ; i >= 0 ; i--){
+            //     Console.WriteLine(numbers[i]);
+            // }
+
+            // foreach (string i in cars)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+
+            Array.Sort(cars);
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+
 
 
 
